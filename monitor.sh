050ebf4253502d2ea76ef42658480f340c18dd4d
jopa <106079878+PajaStef@users.noreply.github.com>
2023-02-26 15:54:23 +0100

    Update monitor.sh

--- monitor.sh
+++ monitor.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-maxla=$(sed -n 's/^[[:space:]]*maxla:[[:space:]]*//p' config/config.yaml)
-maxmem=$(sed -n 's/^[[:space:]]*maxmem:[[:space:]]*//p' config/config.yaml)
+maxla=$(sed -n 's/^[[:space:]]*maxla:[[:space:]]*//p' $HOME/JopasMonitorScript/config/config.yaml)
+maxmem=$(sed -n 's/^[[:space:]]*maxmem:[[:space:]]*//p' $HOME/JopasMonitorScript/config/config.yaml)
 
 loadavg=$(cat /proc/loadavg | awk '{print $1}')
 used_mem=$(free -m | awk '/^Mem:/ {print $3}')
@@ -11,9 +11,9 @@ mem=$(echo "$used_mem > $maxmem" | bc)
 
 
 if [ "$la" -eq 1 ] && [ "$mem" -eq 1  ]; then
-    slackurl=$(sed -n 's/^[[:space:]]*slackurl:[[:space:]]*//p' config.yaml)
-    channel=$(sed -n 's/^[[:space:]]*channel:[[:space:]]*//p' config.yaml)
-    botname=$(sed -n 's/^[[:space:]]*botname:[[:space:]]*//p' config.yaml)
+    slackurl=$(sed -n 's/^[[:space:]]*slackurl:[[:space:]]*//p' $HOME/JopasMonitorScript/config/config.yaml)
+    channel=$(sed -n 's/^[[:space:]]*channel:[[:space:]]*//p' $HOME/JopasMonitorScript/config/config.yaml)
+    botname=$(sed -n 's/^[[:space:]]*botname:[[:space:]]*//p' $HOME/JopasMonitorScript/config/config.yaml)
     message="Load Avg and Memory usage are High! Info: la: $loadavg mem: $used_mem"
     json_payload="payload={
         \"channel\": \"$channel\",
@@ -24,4 +24,3 @@ if [ "$la" -eq 1 ] && [ "$mem" -eq 1  ]; then
     #sending message
     curl --silent -X POST --data-urlencode "$json_payload" "$slackurl"
 fi
-
